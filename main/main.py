c842deb608346897c7dfdd6d07427ebd41568225
Lucas <llovizzio@gmail.com>
2024-02-03 16:58:45 -0300

    .

--- main/main.py
+++ main/main.py
@@ -1,5 +1,6 @@
 import pandas as pd
-df = pd.read_csv("docs\\data.csv", delimiter=';', on_bad_lines='skip')
+
+df = pd.read_csv("main\\docs\\data.csv", sep=";", on_bad_lines="skip")
 
 article_code = "17307" # Validar esto y verificar que existe, de lo contrario dar mensaje de error
 result = df[df['Cód. Artículo'] == article_code]
